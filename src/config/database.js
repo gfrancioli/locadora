a018d75bbc01a4013959bc9ec418c268e8e85570
Gabriel Alves <gabriel.alves@kumulus.com.br>
2021-07-13 15:01:55 -0300

    feat: add database on docker compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+const dotenv = require('dotenv').config();
 module.exports = {
   dialect: 'mysql',
   host: 'localhost',
-  username: 'root',
-  password: 'alo',
-  database: 'movie',
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: false,
     underscored: true,
